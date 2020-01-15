7c462cad1abfe760d0528e26aa7b27b0c0ebbadb
Baran ÖZCAN <drawwbeta@gmail.com>
2020-01-15 14:15:01 +0300

    delete unneeded library

--- src/main/java/me/draww/superrup/utils/Text.java
+++ src/main/java/me/draww/superrup/utils/Text.java
@@ -1,22 +1,11 @@
 package me.draww.superrup.utils;
 
-import net.kyori.text.Component;
-import net.kyori.text.TextComponent;
-import net.kyori.text.adapter.bukkit.TextAdapter;
-import net.kyori.text.serializer.ComponentSerializers;
-import org.bukkit.ChatColor;
-import org.bukkit.command.CommandSender;
-
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
-/**
- * Utilities for working with {@link Component}s and formatted text strings.
- */
-@SuppressWarnings("deprecation")
 public final class Text {
 
     public static final char SECTION_CHAR = '\u00A7'; // §
@@ -28,30 +17,6 @@ public final class Text {
 
     public static String joinNewline(Stream<String> strings) {
         return strings.collect(Collectors.joining("\n"));
-    }
-
-    public static TextComponent fromLegacy(String input, char character) {
-        return ComponentSerializers.LEGACY.deserialize(input, character);
-    }
-
-    public static TextComponent fromLegacy(String input) {
-        return ComponentSerializers.LEGACY.deserialize(input);
-    }
-
-    public static String toLegacy(Component component, char character) {
-        return ComponentSerializers.LEGACY.serialize(component, character);
-    }
-
-    public static String toLegacy(Component component) {
-        return ComponentSerializers.LEGACY.serialize(component);
-    }
-
-    public static void sendMessage(CommandSender sender, Component message) {
-        TextAdapter.sendComponent(sender, message);
-    }
-
-    public static void sendMessage(Iterable<CommandSender> senders, Component message) {
-        TextAdapter.sendComponent(senders, message);
     }
 
     public static String colorize(String s) {
